ac8512203940f03f25620c7dfc0f41390f2ac6c0
risman94 <kamarulrisman1994@gmail.com>
2017-09-08 10:43:48 +0800

    add local auth

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,13 @@ router.get('/login', (req, res) => {
 	res.render('login', { message: req.flash('loginMessage') });
 });
 
+// process the login form
+router.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+}));
+
 router.get('/signup', function(req, res) {
     // render the page and pass in any flash data if it exists
     res.render('signup', { message: req.flash('signupMessage') });
